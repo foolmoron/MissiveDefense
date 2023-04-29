dac73cfa0bd153409fd4cedf6f2708d25394d154
Momin Khan <foolmoron@gmail.com>
2023-04-29 16:30:16 -0400

    some setup

--- scripts/RotateParent.cs
+++ scripts/RotateParent.cs
@@ -1,8 +1,11 @@
 using Godot;
 using System;
 
-public partial class test : Sprite2D
+public partial class RotateParent : Node
 {
+	[Export(PropertyHint.Range, "-720, 720, 1, or_greater, or_lesser")]
+	public float SpeedRad = 360f;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,6 +14,6 @@ public partial class test : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Rotate((float)(delta * 1.2));
+		GetParent<Node2D>().Rotate((float)(delta * Mathf.DegToRad(SpeedRad)));
 	}
 }
